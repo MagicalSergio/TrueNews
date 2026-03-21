1b664d71737e7dc1ffbb484ce84f65e32791f1b0
s.maysky <meg.work.996@gmail.com>
2026-03-21 19:22:41 +0300

    cleanup, code review

--- kommersant_parser.py
+++ kommersant_parser.py
@@ -4,25 +4,25 @@ from selectolax.parser import HTMLParser
 from httpx import AsyncClient
 from news_item import NewsItem
 
-http_client = AsyncClient()
-
 class KommersantParser:
     DOCS_URL = 'https://www.kommersant.ru/doc/'
     NEWS_ROOT_URL = "https://www.kommersant.ru/lenta?from=all_lenta"
     AJAX_REQUEST_URL = "https://www.kommersant.ru/listpage/lazyloaddocs?regionid=77&listtypeid=3&listid=77&date=&intervaltype=&idafter="
     
-    async def get_entites(self, count = 40):
-        try:
-            links = await self._get_ajax_news_links(count)
-            parsing_coroutines = [self._parse_article(l) for l in links]
-            results = await asyncio.gather(*parsing_coroutines)
-            return results
-        except Exception:
-            traceback.print_exc()
-            return []
+    async def get_entities(self, count = 40):
+        async with AsyncClient() as http_client:
+            self.http_client = http_client
+            try:
+                links = await self._get_links(count)
+                parsing_coroutines = [self._parse_article(l) for l in links]
+                results = await asyncio.gather(*parsing_coroutines)
+                return results
+            except Exception:
+                traceback.print_exc()
+                return []
             
     async def _parse_article(self, url):
-        response = await http_client.get(url)
+        response = await self.http_client.get(url)
         tree = HTMLParser(response.text)
         article_node = tree.css_first(f'article[data-article-url="{url}"]')
         title = article_node.css_first('h1').text().replace('\n', '').strip()
@@ -30,23 +30,22 @@ class KommersantParser:
         text = ' '.join([p.text() for p in paragraph_nodes])
         return NewsItem(url, title, text)
             
-    async def _get_ajax_news_links(self, count, delay = 1):
+    async def _get_links(self, count):
         links = await self._get_root_page_links()
         last_link_id = links[-1].split('/')[-1]
         while len(links) < count:
-            # await asyncio.sleep(delay)
             links.extend(await self._get_ajax_links_after(last_link_id))
             last_link_id = links[-1].split('/')[-1]
         return links[:count]
 
     async def _get_root_page_links(self):
-        root_page = await http_client.get(KommersantParser.NEWS_ROOT_URL)
+        root_page = await self.http_client.get(KommersantParser.NEWS_ROOT_URL)
         tree = HTMLParser(root_page.text)
         articles = tree.css('article[data-article-url]')
         return [a.attributes['data-article-url'] for a in articles]
     
     async def _get_ajax_links_after(self, id):
-        response = await http_client.get(self._construct_ajax_req_url(id))
+        response = await self.http_client.get(self._construct_ajax_req_url(id))
         json_data = response.json()
         items = json_data['Items']
         return [self._construct_doc_url_for(i['DocsID']) for i in items]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from kommersant_parser import KommersantParser
 
 async def main():
-    news = await KommersantParser().get_entites()
+    news = await KommersantParser().get_entities()
     json_data = json.dumps([n.__dict__ for n in news], indent=2, ensure_ascii=False)
     with open('news.json', 'w') as f:
         f.write(json_data)
