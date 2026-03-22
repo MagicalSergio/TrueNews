b9428ad1e2cffb4498c8571790b62763c2babefc
s.maysky <meg.work.996@gmail.com>
2026-03-22 03:25:11 +0300

    testing orm

--- src/db/db_conn.py
+++ src/db/db_conn.py
@@ -1,21 +1,31 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, Text, String, ForeignKey
 from sqlalchemy.orm import Session
 
 class DBConn:
     def __init__(self):
+        metadata_obj = MetaData()
+
+        Table(
+            'users_entity', 
+            metadata_obj, 
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('name', String(30), nullable=False),
+            Column('last_name', String(50), nullable=False),
+        )
+        
+        Table(
+            'address_entity',
+            metadata_obj,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('user_id', ForeignKey('users_entity.id'), nullable=False),
+            Column('email_address', String, nullable=False),
+        )    
+        
         self.engine = create_engine('sqlite+pysqlite:///:memory:', echo=True)
+
+        metadata_obj.create_all(self.engine)
+        
         with Session(self.engine) as session:
-            session.execute(text('create table my_table (x int, y int)'))
-            session.commit()
-            
-            session.execute(
-                text('insert into my_table (x, y) values (:x, :y)'),
-                [{ 'x': 1, 'y': 1 }, { 'x': 2, 'y': 4 }],
-            )
-            session.commit()
-            
-            result = session.execute(text('select * from my_table'))
-            ic(result.all())
-            
-            # pass
-            # result = session.
+            result = session.execute(text('select name from sqlite_master where type="table" order by name'))
+            items = result.all()
+            ic(items)
