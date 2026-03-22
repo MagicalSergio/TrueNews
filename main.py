588e598b4b05b83f9135e53fbcf56300af314b1c
s.maysky <meg.work.996@gmail.com>
2026-03-23 00:08:55 +0300

    formatted code

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import asyncio
 import json
 from src.parsers.kommersant_parser import KommersantParser
 
+
 async def main():
     news = await KommersantParser().get_entities()
     json_data = json.dumps([n.__dict__ for n in news], indent=2, ensure_ascii=False)
-    with open('./tmp/news.json', 'w') as f:
+    with open("./tmp/news.json", "w") as f:
         f.write(json_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
