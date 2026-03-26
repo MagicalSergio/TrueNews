c9d49ecf8eadaa13d90f4d70eb7b724ce544c2b7
SuperCoolMan <101731621+iSuperCoolMan@users.noreply.github.com>
2026-03-26 06:02:30 +0400

    init

--- src/db/db_conn.py
+++ src/db/db_conn.py
@@ -1,27 +1,40 @@
-from sqlalchemy import create_engine, Engine
-from icecream import ic
+import re
+
+import importlib
+import pkgutil
+
+from sqlalchemy import create_engine
+
 from src.config import PROJECT_ROOT
-from src.db.tables.main_base import MainBase
-from src.db.tables import *
+from .tables.main_base import MainBase
 from src.util.singleton import singleton
+from .settings import DB_PATH
 
 
 @singleton
 class DBConn:
-    _DB_PATH = PROJECT_ROOT / "data" / "app.db"
     _TABLES_PATH = PROJECT_ROOT / "src" / "db" / "tables"
 
-    _engine: Engine
+    _engine = None
 
     def __init__(self):
         self._check_db_existence()
-        self._engine = create_engine(f"sqlite:///{self._DB_PATH}")
+        self._load_tables()
+        self._engine = create_engine(f"sqlite:///{DB_PATH}")
         MainBase.metadata.create_all(self._engine)
 
     def get_engine(self):
         return self._engine
 
+    def _load_tables(self):
+        modules = [m for m in pkgutil.iter_modules([str(self._TABLES_PATH)])]
+        table_modules = [m for m in modules if m.name.startswith("table_")]
+        table_paths = [f"{self._TABLES_PATH}/{m.name}.py" for m in table_modules]
+        for tp in table_paths:
+            module = re.search(r"(src/).+[^(.py)]", tp).group(0).replace("/", ".")
+            importlib.import_module(module)
+
     def _check_db_existence(self):
-        if not self._DB_PATH.exists():
-            with open(self._DB_PATH, "w"):
+        if not DB_PATH.exists():
+            with open(DB_PATH, "w"):
                 pass
---
+++ src/db/settings.py
@@ -0,0 +1,4 @@
+from src.config import PROJECT_ROOT
+
+
+DB_PATH = PROJECT_ROOT / "data" / "app.db"
